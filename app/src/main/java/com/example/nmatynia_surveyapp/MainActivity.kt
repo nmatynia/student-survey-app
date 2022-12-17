d6056fa5ddb8961eff5330841c0f7ed5b8b37e08
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-12-17 20:50:42 +0100

    feat: simple admin panel

--- app/src/main/java/com/example/nmatynia_surveyapp/MainActivity.kt
+++ app/src/main/java/com/example/nmatynia_surveyapp/MainActivity.kt
@@ -24,23 +24,23 @@ class MainActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
 
         if(login.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Please insert both Login and Password", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Please insert both Login and Password", Toast.LENGTH_LONG).show()
             return
         }
 
         val db = SurveyDataBase(this)
         if(db.getAdmin(login).PassWord === password){
-            val intent = Intent(this, SurveyActivity::class.java)
+            val intent = Intent(this, AdminActivity::class.java)
             startActivity(intent)
-            Toast.makeText(this,"You logged in successfully as an Admin. ", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"You logged in successfully as an Admin. ", Toast.LENGTH_LONG).show()
         }
         else if(db.getStudent(login).PassWord === password){
             val intent = Intent(this, SurveyActivity::class.java)
             startActivity(intent)
-            Toast.makeText(this,"You logged in successfully. ", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"You logged in successfully. ", Toast.LENGTH_LONG).show()
         }
         else {
-            Toast.makeText(this,"Incorrect credentials", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Incorrect credentials", Toast.LENGTH_LONG).show()
         }
     }
 
