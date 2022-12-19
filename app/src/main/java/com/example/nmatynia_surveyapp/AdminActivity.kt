53589198ead3067a8c2b6390f1a7089b62cb655b
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-12-19 20:08:43 +0100

    feat: initial survey creation

--- app/src/main/java/com/example/nmatynia_surveyapp/AdminActivity.kt
+++ app/src/main/java/com/example/nmatynia_surveyapp/AdminActivity.kt
@@ -1,11 +1,17 @@
 package com.example.nmatynia_surveyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+    }
+    fun goToNewSurvey(view: View){
+        val intent = Intent(this, NewSurveyActivity::class.java)
+        startActivity(intent)
     }
 }
